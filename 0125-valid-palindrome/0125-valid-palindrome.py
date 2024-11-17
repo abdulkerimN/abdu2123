53a0108a85d8e68e44bffea18dd1a815936e4dac
abdulkerimN <163136622+abdulkerimN@users.noreply.github.com>
2024-11-17 13:33:17 -0800

    Time: 11 ms (37.38%), Space: 22.3 MB (5.28%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        string=[]
+
+        for i in s:
+            if i.isalnum():
+                string.append(i.lower())
+            else:
+                continue
+        index_comp=(len(string)+1)//2
+        for i in range(index_comp):
+            if string[i]==string[len(string)-1-i]:
+                continue
+            else:
+                return False
+        return True
+
