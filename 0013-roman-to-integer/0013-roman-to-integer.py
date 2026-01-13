442348d594da6bcc953b9b5635a96215796f779f
Abdulkerim Neja <abdualkerim01@gmail.com>
2026-01-13 22:35:41 +0300

    Time: 4 ms (64.71%), Space: 19.5 MB (8.09%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,16 +1,26 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        
-        roman = {"I": 1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
-        
-        n = len(s)
-        result = roman[s[n-1]]
-        
-        for i in range(n-2, -1, -1):
-            
-            if roman[s[i]] < roman[s[i+1]]:
-                result = result - roman[s[i]]
+        roman ={
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total=0
+        preval=0
+
+        for i in reversed(s):
+            curval=roman[i]
+            if curval < preval:
+                total-=curval
             else:
-                result = result + roman[s[i]]
+                total+=curval
+            preval=curval
+
+        return total
+
         
-        return result
